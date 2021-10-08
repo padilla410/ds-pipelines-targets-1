1f799709c51f4a5364eae92e6842a959c498e66e
Julie Padilla <padilla410@gmail.com>
2021-10-08 11:37:37 -0400

    adding arguments for color and pch

--- 2_process/src/prep_data.R
+++ 2_process/src/prep_data.R
@@ -5,26 +5,26 @@
 #' for future plotting
 #' 
 #' @param file_in String, file location for raw ScienceBase data
-#' @param save Boolean, should the prepped data be saved as a csv?
+#' @param col String vector, vector of colors for plotting. One color per model. Model order is pb, dl, pgdl.
+#' @param pch Numeric vector, vector of `pch` values. One pch for each model. Model order is pb, dl, pgdl.
+#' @param file_out String, name and path for processed file
 #' 
-prep_data <- function(file_in, save = TRUE){
+prep_data <- function(file_in, col = c('#1b9e77', '#d95f02', '#7570b3'), pch = c(21:23),  file_out = '2_process/out/model_summary_results.csv'){
   # Prepare the data for plotting
   eval_data <- readr::read_csv(file_in, col_types = 'iccd') %>%
     filter(str_detect(exper_id, 'similar_[0-9]+')) %>%
     mutate(col = case_when(
-      model_type == 'pb' ~ '#1b9e77',
-      model_type == 'dl' ~'#d95f02',
-      model_type == 'pgdl' ~ '#7570b3'
+      model_type == 'pb' ~ col[1],
+      model_type == 'dl' ~ col[2],
+      model_type == 'pgdl' ~ col[3]
     ), pch = case_when(
-      model_type == 'pb' ~ 21,
-      model_type == 'dl' ~ 22,
-      model_type == 'pgdl' ~ 23
+      model_type == 'pb' ~ pch[1],
+      model_type == 'dl' ~ pch[2],
+      model_type == 'pgdl' ~ pch[3]
     ), n_prof = as.numeric(str_extract(exper_id, '[0-9]+')))
   
-  if(save){
-    # Save the processed data
-    readr::write_csv(eval_data, file = file.path('2_process/out/model_summary_results.csv'))
-  }
+  # save output data
+  readr::write_csv(eval_data, file = file_out)
 
   return(eval_data)
 }
