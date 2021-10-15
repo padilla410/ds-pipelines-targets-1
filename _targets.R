3b868ed39e5f80202bb4dcf12ac3b4dfd93cd3fb
Julie Padilla <padilla410@gmail.com>
2021-10-15 13:09:10 -0400

    basic updates to align default _targets.R framework with updated functions

--- _targets.R
+++ _targets.R
@@ -6,30 +6,30 @@ list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    download_data(out_filepath = "model_RMSEs.csv"),
+    fetch_data(path_out = "1_fetch/out/model_RMSEs.csv"),
     format = "file"
   ), 
   # Prepare the data for plotting
   tar_target(
     eval_data,
-    process_data(in_filepath = model_RMSEs_csv),
+    prep_data(file_in = model_RMSEs_csv),
   ),
   # Create a plot
   tar_target(
     figure_1_png,
-    make_plot(out_filepath = "figure_1.png", data = eval_data), 
+    plot_data(data_in = eval_data, file_out = "3_vizualize/out/figure_1.png"),
     format = "file"
   ),
   # Save the processed data
   tar_target(
     model_summary_results_csv,
-    write_csv(eval_data, file = "model_summary_results.csv"), 
+    save_processed_data(data_in = eval_data, file_out = "2_process/out/model_summary_results.csv"),
     format = "file"
   ),
   # Save the model diagnostics
   tar_target(
     model_diagnostic_text_txt,
-    generate_model_diagnostics(out_filepath = "model_diagnostic_text.txt", data = eval_data), 
+    generate_diagnostics(data_in = eval_data, file_out = "2_process/out/model_diagnostic_text.txt"),
     format = "file"
   )
 )
