6f59039c1c200eeb6d0f0e9db7e5ea01938d506f
Julie Padilla <padilla410@gmail.com>
2021-10-15 13:12:33 -0400

    transferring function calls from 00_run.R to code.R

---
+++ code.R
@@ -0,0 +1,6 @@
+# load functions
+source('1_fetch/src/fetch_data.R')
+source('2_process/src/prep_data.R')
+source('2_process/src/save_processed_data.R')
+source('2_process/src/generate_diagnostics.R')
+source('3_vizualize/src/plot_data.R')
